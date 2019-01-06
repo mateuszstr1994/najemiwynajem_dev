61e9c9981dc5ca09b007d8428e8ba21faf3612d4
Mateusz <mateuszstr1994@gmail.com>
2019-01-06 13:08:44 +0100

    Zainstalowany bower + boostrap + jquery

---
+++ src/Controller/HomePageController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomePageController extends AbstractController
+{
+
+    public function index()
+    {  echo 'dassdadas';
+        return $this->render('base.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+}
