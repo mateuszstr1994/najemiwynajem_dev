1d3f7ad12319f9acc0b76a41bf519671c4e7d174
Mateusz <mateuszstr1994@gmail.com>
2019-03-03 16:55:51 +0100

    backup

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -61,49 +61,11 @@ class RegistrationController extends Base
     public function registerAction(Request $request)
     {   
         
-        $user = $this->userManager->createUser();
-        $user->setEnabled(false); 
-
-        $event = new GetResponseUserEvent($user, $request);
-        $this->eventDispatcher->dispatch(FOSUserEvents::REGISTRATION_INITIALIZE, $event);
-
-        if (null !== $event->getResponse()) {
-            return $event->getResponse();
+        if($request->isMethod('POST')) {
+            
         }
-
-        $form = $this->formFactory->createForm();
-        $form->setData($user);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted()) {
-            if ($form->isValid()) {
-                $event = new FormEvent($form, $request);
-                $this->eventDispatcher->dispatch(FOSUserEvents::REGISTRATION_SUCCESS, $event);
-
-                $this->userManager->updateUser($user);
-
-                if (null === $response = $event->getResponse()) {
-                    $url = $this->generateUrl('fos_user_registration_confirmed');
-                    $response = new RedirectResponse($url);
-                }
-
-                $this->eventDispatcher->dispatch(FOSUserEvents::REGISTRATION_COMPLETED, new FilterUserResponseEvent($user, $request, $response));
-
-                return $response;
-            }
-
-            $event = new FormEvent($form, $request);
-            $this->eventDispatcher->dispatch(FOSUserEvents::REGISTRATION_FAILURE, $event);
-
-            if (null !== $response = $event->getResponse()) {
-                return $response;
-            }
-        }
-
-        return $this->render('@FOSUser/Registration/register.html.twig', array(
-            'form' => $form->createView(),
-        ));
+        
+        return $this->render('@FOSUser/Registration/register.html.twig');
     }
 
     /**
