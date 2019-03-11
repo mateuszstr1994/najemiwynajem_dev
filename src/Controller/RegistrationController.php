a6f9a87fecfee68c00da529e29d69bff56ad24f2
Mateusz <mateuszstr1994@gmail.com>
2019-03-11 22:48:24 +0100

    Walidacja rejestracji zrobiona

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -28,6 +28,9 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use FOS\UserBundle\Controller\RegistrationController as Base;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+use App\Application\Sonata\UserBundle\Entity\User;
 /**
  * Controller managing the registration.
  *
@@ -43,14 +46,17 @@ class RegistrationController extends Base
     private $formFactory;
     private $userManager;
     private $tokenStorage;
+    private $validator;
 
-    public function __construct(EventDispatcherInterface $eventDispatcher, FactoryInterface $formFactory, UserManagerInterface $userManager, TokenStorageInterface $tokenStorage)
+    public function __construct(EventDispatcherInterface $eventDispatcher, FactoryInterface $formFactory, UserManagerInterface $userManager, TokenStorageInterface $tokenStorage, ValidatorInterface $validator)
     {
         parent::__construct($eventDispatcher, $formFactory,$userManager, $tokenStorage);
         $this->eventDispatcher = $eventDispatcher;
         $this->formFactory = $formFactory;
         $this->userManager = $userManager;
         $this->tokenStorage = $tokenStorage;
+        $this->validator = $validator;
+        
     }
 
     /**
@@ -58,17 +64,52 @@ class RegistrationController extends Base
      *
      * @return Response
      */
-    public function registerAction(Request $request)
+   public function registerAction(Request $request)
     {   
         
-        $validatorService = $this->get("app.validation.service");
-        
-        if($request->isMethod('POST')) {
-            
-            
+        $user = $this->userManager->createUser();
+        $user->setEnabled(false); 
+
+        $event = new GetResponseUserEvent($user, $request);
+        $this->eventDispatcher->dispatch(FOSUserEvents::REGISTRATION_INITIALIZE, $event);
+
+        if (null !== $event->getResponse()) {
+            return $event->getResponse();
         }
-        
-        return $this->render('@FOSUser/Registration/register.html.twig');
+
+        $form = $this->formFactory->createForm();
+        $form->setData($user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            if ($form->isValid()) {
+                $event = new FormEvent($form, $request);
+                $this->eventDispatcher->dispatch(FOSUserEvents::REGISTRATION_SUCCESS, $event);
+
+                $this->userManager->updateUser($user);
+
+                if (null === $response = $event->getResponse()) {
+                    $url = $this->generateUrl('fos_user_registration_confirmed');
+                    $response = new RedirectResponse($url);
+                }
+
+                $this->eventDispatcher->dispatch(FOSUserEvents::REGISTRATION_COMPLETED, new FilterUserResponseEvent($user, $request, $response));
+
+                return $response;
+            }
+
+            $event = new FormEvent($form, $request);
+            $this->eventDispatcher->dispatch(FOSUserEvents::REGISTRATION_FAILURE, $event);
+
+            if (null !== $response = $event->getResponse()) {
+                return $response;
+            }
+        }
+
+        return $this->render('@FOSUser/Registration/register.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 
     /**
--- src/Service/ValidationService.php
+++ src/Service/ValidationService.php
@@ -4,6 +4,8 @@ namespace App\Service;
 use Doctrine\ORM\EntityManager;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Doctrine\Common\Util\Debug;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 
 class ValidationService{
@@ -19,11 +21,26 @@ class ValidationService{
     }
     
     public function validationPost(array $data, array $fields){
-        
+        foreach($data as $key => $value){
+            if(isset($fields[$key]) && $fields[$key]){ 
+                $this->validationSingleField($value, $fields[$key]);
+            }
+        }
+        return array('status' => 'ok', 'errors' => 'fields');
     }
     
-    public function validationSingleField(string $data, $field){
+    private function validationSingleField(string $data, $fields){
         
+        foreach($fields as $key => $value){
+            if($key == 'minLength'){
+                return 'Musze dać plik i numer błedu';
+            }
+        }
+    }
+    
+    private function valiatorMinLength($data, $minLength){
+        if(strlen($data) < $minLength )
+            echo 'assaas';
     }
    
 }
