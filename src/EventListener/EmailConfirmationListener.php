c8efa0162b167eb4f4fb35ecf320caad12f6161a
Mateusz <mateuszstr1994@gmail.com>
2019-04-07 19:32:59 +0200

    Napisanie fos_user.registration.success

---
+++ src/EventListener/EmailConfirmationListener.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * This file is part of the FOSUserBundle package.
+ *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\EventListener;
+
+use FOS\UserBundle\Event\FormEvent;
+use FOS\UserBundle\FOSUserEvents;
+use FOS\UserBundle\Mailer\MailerInterface;
+use FOS\UserBundle\Util\TokenGeneratorInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use FOS\UserBundle\EventListener\EmailConfirmationListener as BaseEmailConfirmationListener;
+
+class EmailConfirmationListener extends BaseEmailConfirmationListener
+{
+    public function onRegistrationSuccess(FormEvent $event)
+    {
+        /** @var $user \FOS\UserBundle\Model\UserInterface */
+        $user = $event->getForm()->getData();
+
+        $user->setEnabled(false);
+        if (null === $user->getConfirmationToken()) {
+            $user->setConfirmationToken($this->tokenGenerator->generateToken());
+        }
+
+        $this->mailer->sendConfirmationEmailMessage($user);
+
+        $this->session->set('fos_user_send_confirmation_email/email', $user->getEmail());
+
+        $url = $this->router->generate('fos_user_registration_check_email');
+        $event->setResponse(new RedirectResponse($url));
+    }
+}
