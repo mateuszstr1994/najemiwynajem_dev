dfc1a02d00527cdce973b4b33edd6575d44ab2e7
Mateusz <mateuszstr1994@gmail.com>
2019-04-23 20:38:20 +0200

    Dodanie CountDown i redirecta przy potwierdzeniu emaila.

--- assets/js/mainAngular.js
+++ assets/js/mainAngular.js
@@ -4,10 +4,36 @@
  * and open the template in the editor.
  */
 
-var APP = angular.module("niwApp", [])
+var APP = angular.module("niwApp", []).config(function($interpolateProvider){
+    $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
+});
 
 APP.controller("checkEmailController", function($scope, $http) {
     console.log('checkEmailController');
 });
+
+APP.controller("fosUserConfirmedController", ['$scope','$timeout', function($scope, $timeout) {
+    console.log('fosUserConfirmedController');
+    var stopped;
+    $scope.counter = 15;
+    $scope.redirectUrl =  angular.element('#redirectUrl').attr('href');
+    $scope.countdown = function() {
+        stopped = $timeout(function() {
+            $scope.counter--;
+            $scope.countdown();
+        }, 1000);
+
+        if($scope.counter < 1) {
+            $timeout.cancel(stopped);
+            angular.element('#clearAfterCountDown').remove();
+            $scope.counter = 'Przekierowywanie';
+            window.location.replace($scope.redirectUrl);
+        }
+
+    };
+
+}]);
+
+
 
 
